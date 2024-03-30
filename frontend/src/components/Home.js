5bd1d7a150f9d9910a779d446ae37bb2e5af1bc8
UnnatiH <unnatihassanandani08@gmail.com>
2024-03-31 00:21:40 +0530

    front end added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,34 +3,55 @@ import './Home.css'
 import logoblack from './Optimaoptima black .png'
 import {Circle} from 'react-awesome-shapes'
 import {Donut} from 'react-awesome-shapes'
+import { useNavigate } from "react-router-dom"
 function Home()
 {
+    const navigate = useNavigate();
+ 
+    const LoginPage = () => {
+        navigate("/login");
+    }
+    const SignPage = () => {
+        navigate("/signup");
+    }
     return (<>
     {/* <img src={Backimg} alt="landing img" className="Backimg"/>
     <div className="Divvv" style={style1}>
     <img src={Backimg} alt="landing img" className="Backimg"/>
     <img src={logoblack} className="Logogreen"/>
     </div> */}
+    <div class="Homebg">
     <Circle
     color="linear-gradient(135deg, #DAFFD0, #21A700)"
-    size={['150px', '150px', '180px', '180px']}
+    size={['250px', '250px', '250px', '250px']}
     zIndex={2} /> 
-    <div><center><img src={logoblack} alt="logo black"/> </center></div>
+    <div style={{marginLeft:"1700px", marginTop:"-50px"}}><Donut
+    color="#21A700"
+    size="400px"
+    width={['40px', '40px', '60px', '60px']}
+    zIndex={2}
+/> </div>
+    <div><center><img src={logoblack} alt="logo black" style={{marginTop:"100px"}}/> </center></div>
     <div>
     <center>
-    <button class='Signupbttn'>Sign Up</button>
-    <button class='Loginbttn'>Login</button>
+    <button class='Signupbttn' onClick={SignPage}>Sign Up</button>
+    <button class='Loginbttn' onClick={LoginPage}>Login</button>
     </center>
     </div>
     <br/>
-   <div><h3 class="line-1 anim-typewriter">Optima: Real-Time Action Detection for Healthier Lives</h3></div>
-   <div><center><button class="bounce">Get started with guided tour</button></center></div>
-   <Donut
+   <center><div><h3 class="line-1 anim-typewriter">Optima: Real-Time Action Detection for Healthier Lives</h3></div> </center>
+   <div><center><button class="bounce" onClick={SignPage}>Get started with guided tour</button></center></div>
+   <div style={{marginLeft:"200px"}}><Donut
     color="#21A700"
     size="180px"
     width={['40px', '40px', '60px', '60px']}
     zIndex={2}
-/> 
+/> </div>
+<div style={{marginLeft:"1200px"}}><Circle
+    color="linear-gradient(135deg, #DAFFD0, #21A700)"
+    size={['450px', '450px', '450px', '450px']}
+    zIndex={2} />  </div>
+</div>
     </>);
 };
 
